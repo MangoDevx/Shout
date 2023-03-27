85048af37c89bc76c101fe7f58cd1363b410ac7a
kfabian12 <kfabian5178@gmail.com>
2023-03-27 15:19:09 -0400

    loginPage

--- Frontend/lib/src/views/loginview.dart
+++ Frontend/lib/src/views/loginview.dart
@@ -3,12 +3,85 @@ import 'package:flutter/material.dart';
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
+  TextEditingController usernameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: const [
-        Text('TBD'),
-      ],
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: _page(),
+    );
+  }
+
+  Widget _page() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            _inputBox("Username", usernameController),
+            const SizedBox(height: 25),
+            _inputBox("Password", passwordController, isPassword: true),
+            const SizedBox(height: 25),
+            _loginButton(),
+            const SizedBox(height: 25),
+            _bottomText(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _inputBox(String hintText, TextEditingController controller,
+      {isPassword = false}) {
+    var border = OutlineInputBorder(
+        borderRadius: BorderRadius.circular(18),
+        borderSide: const BorderSide(color: Colors.deepPurple));
+    var errorBorder = OutlineInputBorder(
+        borderRadius: BorderRadius.circular(18),
+        borderSide: const BorderSide(color: Colors.red));
+    return TextField(
+      style: const TextStyle(color: Colors.white),
+      controller: controller,
+      decoration: InputDecoration(
+        hintText: hintText,
+        hintStyle: const TextStyle(color: Colors.white),
+        border: border,
+        errorBorder: errorBorder,
+      ),
+      obscureText: isPassword,
+    );
+  }
+
+  Widget _loginButton() {
+    return ElevatedButton(
+      onPressed: () {
+        debugPrint("Username : ${usernameController.text}");
+        debugPrint("Password : ${passwordController.text}");
+      },
+      style: ElevatedButton.styleFrom(
+        shape: const StadiumBorder(),
+        backgroundColor: Colors.deepPurple,
+        padding: const EdgeInsets.symmetric(vertical: 16),
+        textStyle: const TextStyle(color: Colors.white),
+      ),
+      child: const SizedBox(
+          width: double.infinity,
+          child: Text(
+            "Login",
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 18),
+          )),
+    );
+  }
+
+  Widget _bottomText() {
+    return const Text(
+      "Forgot Password? Click Here!",
+      textAlign: TextAlign.center,
+      style: TextStyle(fontSize: 12, color: Colors.black),
     );
   }
 }
