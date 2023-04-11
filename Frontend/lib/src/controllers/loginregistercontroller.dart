a3c5983d564ad52434ed6272e2bae002886e7ad0
Keith <49873540+MangoDevx@users.noreply.github.com>
2023-04-10 23:23:54 -0400

    Updated login/register controller

---
+++ Frontend/lib/src/controllers/loginregistercontroller.dart
@@ -0,0 +1,8 @@
+bool sendLoginCredentials(String username, String password) {
+  return false;
+  // TODO: Send backend request
+}
+
+bool sendRegistrationCredentials(String username, String password, String phoneNumber) {
+
+}
