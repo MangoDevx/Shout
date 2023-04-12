399841721fb8ef8fde34140da9e48b585e0eca29
Keith <49873540+MangoDevx@users.noreply.github.com>
2023-04-12 00:59:33 -0400

    Fixed scroll overflow

--- Frontend/lib/src/views/loginview.dart
+++ Frontend/lib/src/views/loginview.dart
@@ -45,126 +45,132 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Theme.of(context).backgroundColor,
+        resizeToAvoidBottomInset: true,
         body: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const SizedBox(
-                  height: 150,
-                  width: 150,
-                  child: DecoratedBox(
-                      decoration: BoxDecoration(
-                          image: DecorationImage(
-                    image: AssetImage('assets/shouttplogo.png'),
-                  ))),
-                ),
-                const Padding(
-                  padding: EdgeInsets.fromLTRB(0, 25, 0, 0),
-                ),
-                Form(
-                  key: _formKey,
-                  onChanged: () {
-                    Form.of(primaryFocus!.context!)!.validate();
-                  },
-                  child: Wrap(
+            padding: const EdgeInsets.all(8.0),
+            child: SingleChildScrollView(
+              child: Container(
+                height: MediaQuery.of(context).size.height,
+                width: MediaQuery.of(context).size.width,
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      TextFormField(
-                        style: TextStyle(
-                            color: Theme.of(context).primaryColorDark),
-                        decoration: InputDecoration(
-                          icon: Icon(Icons.person_pin,
-                              color: Theme.of(context).primaryColor),
-                          hintText: 'Username',
-                          hintStyle:
-                              TextStyle(color: Theme.of(context).primaryColor),
-                          labelText: 'Username',
-                          labelStyle: TextStyle(
-                              color: Theme.of(context).primaryColorDark),
-                          enabledBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(
-                                width: 3,
-                                color: Theme.of(context).primaryColor),
-                          ),
-                        ),
-                        onSaved: (String? username) {
-                          // Sent when a form is saved
-                          if (username != null) {
-                            this.username = username;
-                          }
-                        },
-                        validator: (String? username) {
-                          // Validation checks go here
-                          // I.E we don't want @'s or weird characters in names
-                          if (username != null && username.length > 1) {
-                            _formKey.currentState!.save();
-                          }
-                        },
+                      const SizedBox(
+                        height: 150,
+                        width: 150,
+                        child: DecoratedBox(
+                            decoration: BoxDecoration(
+                                image: DecorationImage(
+                          image: AssetImage('assets/shouttplogo.png'),
+                        ))),
                       ),
-                      TextFormField(
-                        // TODO: Blur / star out password text
-                        style: TextStyle(
-                            color: Theme.of(context).primaryColorDark),
-                        decoration: InputDecoration(
-                          icon: Icon(Icons.lock,
-                              color: Theme.of(context).primaryColor),
-                          iconColor: Theme.of(context).primaryColor,
-                          hintText: 'Password',
-                          hintStyle:
-                              TextStyle(color: Theme.of(context).primaryColor),
-                          labelText: 'Password',
-                          labelStyle: TextStyle(
-                              color: Theme.of(context).primaryColorDark),
-                          enabledBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(
-                                width: 3,
-                                color: Theme.of(context).primaryColor),
-                          ),
-                        ),
-                        onSaved: (String? password) {
-                          // Sent when a form is saved
-                          if (password != null) {
-                            this.password = password;
-                          }
-                        },
-                        validator: (String? password) {
-                          // Validation checks go here
-                          if (password != null && password.length > 1) {
-                            _formKey.currentState!.save();
-                          }
-                        },
+                      const Padding(
+                        padding: EdgeInsets.fromLTRB(0, 25, 0, 0),
                       ),
+                      Form(
+                        key: _formKey,
+                        onChanged: () {
+                          Form.of(primaryFocus!.context!)!.validate();
+                        },
+                        child: Wrap(
+                          children: [
+                            TextFormField(
+                              style: TextStyle(
+                                  color: Theme.of(context).primaryColorDark),
+                              decoration: InputDecoration(
+                                icon: Icon(Icons.person_pin,
+                                    color: Theme.of(context).primaryColor),
+                                hintText: 'Username',
+                                hintStyle: TextStyle(
+                                    color: Theme.of(context).primaryColor),
+                                labelText: 'Username',
+                                labelStyle: TextStyle(
+                                    color: Theme.of(context).primaryColorDark),
+                                enabledBorder: UnderlineInputBorder(
+                                  borderSide: BorderSide(
+                                      width: 3,
+                                      color: Theme.of(context).primaryColor),
+                                ),
+                              ),
+                              onSaved: (String? username) {
+                                // Sent when a form is saved
+                                if (username != null) {
+                                  this.username = username;
+                                }
+                              },
+                              validator: (String? username) {
+                                // Validation checks go here
+                                // I.E we don't want @'s or weird characters in names
+                                if (username != null && username.length > 1) {
+                                  _formKey.currentState!.save();
+                                }
+                              },
+                            ),
+                            TextFormField(
+                              // TODO: Blur / star out password text
+                              style: TextStyle(
+                                  color: Theme.of(context).primaryColorDark),
+                              decoration: InputDecoration(
+                                icon: Icon(Icons.lock,
+                                    color: Theme.of(context).primaryColor),
+                                iconColor: Theme.of(context).primaryColor,
+                                hintText: 'Password',
+                                hintStyle: TextStyle(
+                                    color: Theme.of(context).primaryColor),
+                                labelText: 'Password',
+                                labelStyle: TextStyle(
+                                    color: Theme.of(context).primaryColorDark),
+                                enabledBorder: UnderlineInputBorder(
+                                  borderSide: BorderSide(
+                                      width: 3,
+                                      color: Theme.of(context).primaryColor),
+                                ),
+                              ),
+                              onSaved: (String? password) {
+                                // Sent when a form is saved
+                                if (password != null) {
+                                  this.password = password;
+                                }
+                              },
+                              validator: (String? password) {
+                                // Validation checks go here
+                                if (password != null && password.length > 1) {
+                                  _formKey.currentState!.save();
+                                }
+                              },
+                            ),
+                          ],
+                        ),
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.fromLTRB(0, 25, 0, 0),
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          _sendCredentials();
+                        },
+                        child: const SizedBox(
+                            width: double.infinity,
+                            child: Text(
+                              "Login",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(fontSize: 18),
+                            )),
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.fromLTRB(0, 25, 0, 0),
+                      ),
+                      register(),
+                      const Padding(
+                        padding: EdgeInsets.fromLTRB(0, 25, 0, 0),
+                      ),
+                      _errorText(errorOutput),
                     ],
                   ),
                 ),
-                const Padding(
-                  padding: EdgeInsets.fromLTRB(0, 25, 0, 0),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    _sendCredentials();
-                  },
-                  child: const SizedBox(
-                      width: double.infinity,
-                      child: Text(
-                        "Login",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(fontSize: 18),
-                      )),
-                ),
-                const Padding(
-                  padding: EdgeInsets.fromLTRB(0, 25, 0, 0),
-                ),
-                register(),
-                const Padding(
-                  padding: EdgeInsets.fromLTRB(0, 25, 0, 0),
-                ),
-                _errorText(errorOutput),
-              ],
-            ),
-          ),
-        ));
+              ),
+            )));
   }
 
   Widget register() {
