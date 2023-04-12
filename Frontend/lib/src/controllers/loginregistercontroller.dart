cdb99ba0dcdd69b9ac631d3cf8f91dd89513df34
Keith <49873540+MangoDevx@users.noreply.github.com>
2023-04-11 23:57:06 -0400

    Updated login controller to return a proper res.

--- Frontend/lib/src/controllers/loginregistercontroller.dart
+++ Frontend/lib/src/controllers/loginregistercontroller.dart
@@ -1,23 +1,17 @@
 import 'dart:convert';
+import 'package:frontend/src/models/authresponsemodel.dart';
 import 'package:http/http.dart' as http;
-import 'package:frontend/src/models/loginpayload.dart';
+import 'package:frontend/src/models/loginauthmodel.dart';
 
-Future<int> sendLoginCredentials(String username, String password) async {
-  LoginPayload payload = LoginPayload(username: username, password: password);
+Future<AuthResponseModel> sendLoginCredentials(String username, String password) async {
+  LoginAuthModel payload = LoginAuthModel(username: username, password: password);
   String jsonPayload = jsonEncode(payload);
 
   // TODO: This really shouldn't be static but since its a small project right now, fix in future if desired
   var url = Uri.https("shoutmessaging.azurewebsites.net", "api/auth/login");
   var response = await http.post(url, body: jsonPayload, headers: {"Content-Type": "application/json"});
 
-  if(response.statusCode == 200) {
-    return 200;
-  }
-  else {
-    // TODO: Remove print in production
-    print(response.body);
-    return response.statusCode;
-  }
+  return AuthResponseModel(statusCode: response.statusCode, responseBody: response.body);
 }
 
 bool sendRegistrationCredentials(String username, String password, String phoneNumber) {
