50132081f5e9579d7f4d28b68afc99fc2dfdcc9b
Keith <49873540+MangoDevx@users.noreply.github.com>
2023-04-12 15:31:47 -0400

    Added some TODOs to the homeview

--- Frontend/lib/src/views/homeview.dart
+++ Frontend/lib/src/views/homeview.dart
@@ -13,10 +13,53 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return WillPopScope(
       onWillPop: () async => false,
-      child: const Text(
-        'Logged in - Home Page WIP'
+      child: Scaffold (
+        appBar: homeAppBar(),
+        bottomNavigationBar: homeAppBar(), // TODO: Make this the text input area, etc
+        body: SizedBox.expand(
+          child: ListView(
+            // TODO : Display messages
+          ),
+        ),
       ),
     );
   }
-
+  
+  Widget homeAppBar() { 
+    // TODO: Design top bar
+    return AppBar(
+        title: const Text('AppBar Demo'),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.add_alert),
+            tooltip: 'Show Snackbar',
+            onPressed: () {
+              ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(content: Text('This is a snackbar')));
+            },
+          ),
+          IconButton(
+            icon: const Icon(Icons.navigate_next),
+            tooltip: 'Go to the next page',
+            onPressed: () {
+              Navigator.push(context, MaterialPageRoute<void>(
+                builder: (BuildContext context) {
+                  return Scaffold(
+                    appBar: AppBar(
+                      title: const Text('Next page'),
+                    ),
+                    body: const Center(
+                      child: Text(
+                        'This is the next page',
+                        style: TextStyle(fontSize: 24),
+                      ),
+                    ),
+                  );
+                },
+              ));
+            },
+          ),
+        ],
+      );
+  }
 }
