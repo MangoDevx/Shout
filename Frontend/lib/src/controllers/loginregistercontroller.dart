e28ed7e907764092b9f64d2ba0a3de963052f0e0
Keith <49873540+MangoDevx@users.noreply.github.com>
2023-04-12 20:00:09 -0400

    Refactor

--- Frontend/lib/src/controllers/loginregistercontroller.dart
+++ Frontend/lib/src/controllers/loginregistercontroller.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:frontend/src/models/securestorage.dart';
 import 'package:http/http.dart' as http;
 import 'package:frontend/src/models/authresponsemodel.dart';
